6df801345592bc94e6da8c0725ed0e143a31ede3
Mahesh <umamaheshbatraju@gmail.com>
2025-01-11 22:46:11 +0530

    Added some files

--- day-1/main.tf
+++ day-1/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "name" {
   ami = ""
   instance_type = "t2.micro"
   key_name = "Mahi"
+  subnet_id = ""
 
   tags = {
     Name = "Server1" #Adding this tag to get more clearness
